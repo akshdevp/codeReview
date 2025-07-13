7c18a5f5b5b6a627e982e233113c1db21b5a3f9e
akshith reddy <akshithreddy@akshiths-MBP.attlocal.net>
2025-07-13 00:11:49 -0400

    added the code

--- review/review_runner.py
+++ review/review_runner.py
@@ -1,9 +1,9 @@
-from review.utils import get_changed_java_files, read_file_content
+from review.utils import get_all_java_files, read_file_content
 from review.analyzer import analyze_java_code
 
 def run_review():
-    java_files = get_changed_java_files()
-    print("🟨 Java files changed:", java_files)
+    java_files = get_all_java_files()
+    print("Java files changed:", java_files)
 
     for file in java_files:
         code = read_file_content(file)
--- review/utils.py
+++ review/utils.py
@@ -1,12 +1,12 @@
-import subprocess
+import os
 
-def get_changed_java_files():
-    result = subprocess.run(
-        ["git", "diff", "--name-only", "HEAD~1"],
-        capture_output=True,
-        text=True
-    )
-    return [f for f in result.stdout.splitlines() if f.endswith(".java")]
+def get_all_java_files():
+    java_files = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(".java"):
+                java_files.append(os.path.join(root, file))
+    return java_files
 
 def read_file_content(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
